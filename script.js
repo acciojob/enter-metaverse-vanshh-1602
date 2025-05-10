fdfa60e281f05df8ab51d84c116ccd9e246bc352
Vansh Bhadoria <115222279+vanshh-1602@users.noreply.github.com>
2025-05-10 20:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const pt = document.getElementById('txt');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+	pt.innerText = "Entered Metaverse";
+});
