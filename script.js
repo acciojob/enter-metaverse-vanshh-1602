e090a269bf364ff3b79b333dd4eabc71814d5b5b
Vansh Bhadoria <115222279+vanshh-1602@users.noreply.github.com>
2025-05-15 05:55:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
 const pt = document.getElementById('txt');
 const btn = document.getElementById('btn');
+const newHeading = document.createElement('h1');
+newHeading.textContent = 'Entered Metaverse';
 
 btn.addEventListener('click', () => {
-	pt.innerText = "Entered Metaverse";
+	pt.replaceWith(newHeading);
 });
